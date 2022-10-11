e13e83e5bde99a46b6363180ab9b22fab61675fa
Lee Seong Jun <devel0624@gmail.com>
2022-10-11 14:30:36 +0900

    create destory-method

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/EmailMessageSender.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/EmailMessageSender.java
@@ -3,6 +3,9 @@ package com.nhnacademy.edu.springframework.messagesender;
 import com.nhnacademy.edu.springframework.User;
 
 public class EmailMessageSender implements MessageSender{
+    public void destroyMethod(){
+        System.out.println("Destroy Method on Email Message Sender");
+    }
     @Override
     public void sendMessage(User user, String message) {
         System.out.println("Email Message Sent to ${email} : ${message}");
--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/SmsMessageSender.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/SmsMessageSender.java
@@ -3,6 +3,9 @@ package com.nhnacademy.edu.springframework.messagesender;
 import com.nhnacademy.edu.springframework.User;
 
 public class SmsMessageSender implements MessageSender {
+    public void destroyMethod(){
+        System.out.println("Destroy Method on SMS Message Sender");
+    }
     @Override
     public void sendMessage(User user, String message) {
         System.out.println("SMS Message Sent to ${phoneNumber} : ${message}");
