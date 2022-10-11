30e4517861449eee64add9dc8dd5fded5c6ab4d3
Lee Seong Jun <devel0624@gmail.com>
2022-10-11 14:17:12 +0900

    create init-method

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/EmailMessageSender.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/EmailMessageSender.java
@@ -3,6 +3,10 @@ package com.nhnacademy.edu.springframework.messagesender;
 import com.nhnacademy.edu.springframework.User;
 
 public class EmailMessageSender implements MessageSender{
+
+    public void initMethod(){
+        System.out.println("Init Method on Email Message");
+    }
     @Override
     public void sendMessage(User user, String message) {
         System.out.println("Email Message Sent to ${email} : ${message}");
--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/SmsMessageSender.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/SmsMessageSender.java
@@ -3,6 +3,9 @@ package com.nhnacademy.edu.springframework.messagesender;
 import com.nhnacademy.edu.springframework.User;
 
 public class SmsMessageSender implements MessageSender {
+    public void initMethod(){
+        System.out.println("Init Method on SMS Message");
+    }
     @Override
     public void sendMessage(User user, String message) {
         System.out.println("SMS Message Sent to ${phoneNumber} : ${message}");
