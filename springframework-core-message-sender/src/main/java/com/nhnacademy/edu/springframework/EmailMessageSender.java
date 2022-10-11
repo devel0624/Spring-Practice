675d1f566498350d8444bd9fb202e71c9b2c9877
Lee Seong Jun <devel0624@gmail.com>
2022-10-11 11:05:13 +0900

    email.java : phoneNumber -> email

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/EmailMessageSender.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/EmailMessageSender.java
@@ -3,6 +3,6 @@ package com.nhnacademy.edu.springframework;
 public class EmailMessageSender implements MessageSender{
     @Override
     public void sendMessage(User user, String message) {
-        System.out.println("Email Message Sent to ${phoneNumber} : ${message}");
+        System.out.println("Email Message Sent to ${email} : ${message}");
     }
 }
